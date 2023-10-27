19b30caf7efdf9e50d220f21ccdcc0ea513bc4dd
emariif <okbro8788@gmail.com>
2023-10-27 08:12:44 +0700

    detail page

--- lib/widgets/home_popular_item.dart
+++ lib/widgets/home_popular_item.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:space/theme.dart';
 
 class HomePopularItem extends StatelessWidget {
-  String imageUrl;
-  String title;
-  int price;
-  bool isWishlist;
-
   HomePopularItem(
       {Key? key,
       required this.imageUrl,
@@ -15,71 +10,81 @@ class HomePopularItem extends StatelessWidget {
       required this.isWishlist})
       : super(key: key);
 
+  String imageUrl;
+  bool isWishlist;
+  int price;
+  String title;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          margin: const EdgeInsets.only(left: 24),
-          child: Column(
-            children: [
-              Container(
-                width: 200,
-                height: 180,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.vertical(
-                    top: Radius.circular(10),
-                  ),
-                  color: kWhiteGreyColor,
-                ),
-                child: Center(
-                  child: Image.asset(
-                    imageUrl,
-                    width: 160,
-                  ),
-                ),
-              ),
-              Container(
-                width: 200,
-                padding: const EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 20,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: blackTextStyle.copyWith(
-                        fontWeight: semiBold,
-                        fontSize: 18,
-                      ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail');
+      },
+      child: SizedBox(
+        height: 300,
+        child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: const EdgeInsets.only(left: 24),
+            child: Column(
+              children: [
+                Container(
+                  width: 200,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(10),
                     ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '\$$price',
-                          style: blackTextStyle.copyWith(
-                              fontWeight: semiBold, fontSize: 18),
+                    color: kWhiteGreyColor,
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      imageUrl,
+                      width: 160,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12,
+                    horizontal: 20,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: blackTextStyle.copyWith(
+                          fontWeight: semiBold,
+                          fontSize: 18,
                         ),
-                        Image.asset(
-                          isWishlist
-                              ? 'assets/button_wishlist_active.png'
-                              : 'assets/button_wishlist.png',
-                          width: 44,
-                        )
-                      ],
-                    ),
-                  ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '\$$price',
+                            style: blackTextStyle.copyWith(
+                                fontWeight: semiBold, fontSize: 18),
+                          ),
+                          Image.asset(
+                            isWishlist
+                                ? 'assets/button_wishlist_active.png'
+                                : 'assets/button_wishlist.png',
+                            width: 44,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          )),
+              ],
+            )),
+      ),
     );
   }
 }
